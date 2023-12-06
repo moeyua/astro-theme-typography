406d1603f50e65628ce44de2394926c1bbdae698
Moeyua <moeyua13@icloud.com>
2023-12-06 14:57:05 +0800

    🎈 perf: import site from theme.config

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
+import { THEME_CONFIG } from "./src/theme.config";
+
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-theme-typography.vercel.app',
+  site: THEME_CONFIG.website,
   redirects: {
     '/': '/home/1',
     '/home': '/home/1'
