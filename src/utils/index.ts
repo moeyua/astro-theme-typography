87aaf798e9bd6d17080fa700984aeaeca4da8050
Moeyua <moeyua13@icloud.com>
2023-12-08 17:45:21 +0800

    Refactor code to use getPostDescription function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 import { getCollection } from 'astro:content'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
 
 export async function getCategories() {
   const posts = await getPosts()
@@ -22,4 +24,16 @@ export async function getPosts() {
   const posts = await getCollection('posts')
   posts.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime())
   return posts
+}
+
+const parser = new MarkdownIt()
+
+export function getPostDescription(post: Post) {
+  if (post.data.description) {
+    return post.data.description
+  }
+
+  const html = parser.render(post.body)
+  const sanitized = sanitizeHtml(html, { allowedTags: [] })
+  return sanitized.slice(0, 400)
 }
