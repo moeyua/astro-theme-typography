9f4278ba1239c2f5f76692eadfe095171ceca38d
suzulang <suzulang.jiang@gmail.com>
2024-03-30 15:52:58 +0800

    修改seo标题

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,6 +1,6 @@
 export const THEME_CONFIG: App.Locals['config'] = {
   /** blog title */
-  title: "活版印字",
+  title: "人生現役",
   /** your name */
   author: "Typography",
   /** website description */
