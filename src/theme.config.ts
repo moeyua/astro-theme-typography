558213d844dce7fec41b7448ef25f8fd9a60b33a
xynay <xiregiji@gmail.com>
2024-08-09 13:42:04 +0800

    Update theme.config.ts

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -6,7 +6,7 @@ export const THEME_CONFIG: App.Locals['config'] = {
   /** website description */
   desc: "欢迎来到梅花易博客！这是一个个人博客，记录生活点滴与成长足迹，同时分享我对易经的热爱与研究心得。作为一名易学爱好者，我在这里探讨中华传统文化中的智慧与奥秘，提供深入浅出的易经解读和实用的生活建议。无论您是对易学感兴趣，还是希望了解我的生活故事，梅花易博客都将带给您丰富的内容与独特的视角。",
   /** your deployed domain */
-  website: "https://astro-theme-typography-sage.vercel.app/",
+  website: "https://meihuayi.com/",
   /** your locale */
   locale: "zh-cn",
   /** theme style */
@@ -47,7 +47,7 @@ export const THEME_CONFIG: App.Locals['config'] = {
   ],
   /** your category name mapping, which the `path` will be shown in the url */
   category_map: [
-    { name: "胡适", path: "hu-shi" },
+    { name: "网络基础", path: "NetworkFoundations" },
   ],
   /** your comment provider */
   comments: {
