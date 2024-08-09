ec19fbec16c64dcccf9c48ddebe277760d0f5798
xynay <xiregiji@gmail.com>
2024-08-09 13:21:39 +0800

    0

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,16 +1,16 @@
 export const THEME_CONFIG: App.Locals['config'] = {
   /** blog title */
-  title: "活版印字",
+  title: "梅花易博客",
   /** your name */
-  author: "Typography",
+  author: "梅花易",
   /** website description */
-  desc: "Rediscory the beauty of typography",
+  desc: "欢迎来到梅花易博客！这是一个个人博客，记录生活点滴与成长足迹，同时分享我对易经的热爱与研究心得。作为一名易学爱好者，我在这里探讨中华传统文化中的智慧与奥秘，提供深入浅出的易经解读和实用的生活建议。无论您是对易学感兴趣，还是希望了解我的生活故事，梅花易博客都将带给您丰富的内容与独特的视角。",
   /** your deployed domain */
-  website: "https://astro-theme-typography.vercel.app/",
+  website: "https://astro-theme-typography-sage.vercel.app/",
   /** your locale */
-  locale: "en-us",
+  locale: "zh-cn",
   /** theme style */
-  themeStyle: "light",
+  themeStyle: "auto",
   /** your socials */
   socials: [
     {
@@ -21,18 +21,10 @@ export const THEME_CONFIG: App.Locals['config'] = {
       name: "rss",
       href: "/atom.xml",
     },
-    {
-      name: "twitter",
-      href: "https://github.com/moeyua/astro-theme-typography",
-    },
-    {
-      name: "mastodon",
-      href: "https://github.com/moeyua/astro-theme-typography",
-    }
   ],
   /** your header info */
   header: {
-    twitter: "@moeyua13",
+    // twitter: "@moeyua13",
   },
   /** your navigation links */
   navs: [
@@ -59,10 +51,10 @@ export const THEME_CONFIG: App.Locals['config'] = {
   ],
   /** your comment provider */
   comments: {
-    disqus: {
-      // please change this to your disqus shortname
-      shortname: "typography-astro",
-    },
+    // disqus: {
+    //   // please change this to your disqus shortname
+    //   shortname: "typography-astro",
+    // },
     // giscus: {
     //   repo: 'moeyua/astro-theme-typography',
     //   repoId: 'R_kgDOKy9HOQ',
