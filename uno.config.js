a8e4d4182683c60dd0c63ba2b22d2dea4ecbe406
Moeyua <mail@moeyua.com>
2025-07-16 19:21:42 +0800

    chore: 更新依赖到 major 版本

--- uno.config.js
+++ uno.config.js
@@ -4,7 +4,7 @@ import {
   defineConfig,
   presetIcons,
   presetTypography,
-  presetUno,
+  presetWind3,
   transformerVariantGroup,
 } from 'unocss'
 import presetTheme from 'unocss-preset-theme'
@@ -54,7 +54,7 @@ export default defineConfig({
     ],
   ],
   presets: [
-    presetUno(),
+    presetWind3(),
     presetTypography({ cssExtend }),
     presetAttributify(),
     presetIcons({ scale: 1.2, warn: true }),
